cd173ba19de2df0ed797eeb208cf3f63e272d086
TheSaviourEking <saviour.eking@outlook.com>
2023-02-16 00:59:03 -0800

    adds 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,7 @@
 */
 int main(void)
 {
-    char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    write(STDERR, msg, sizeof(msg)-1);
-    return 1;
+	char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(STDERR, msg, sizeof(msg) - 1);
+return (1);
 }
