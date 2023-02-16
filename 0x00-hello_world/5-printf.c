eec2b1c50d0fa5dd0c196bd3c0a211e028d740da
TheSaviourEking <saviour.eking@outlook.com>
2023-02-16 00:32:01 -0800

    adds 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints out parenthesized text
+ * Description: prints out "with proper grammar, but the outcome
+ * is a piece of art,, followed by a new line".(
+ * Do not make a line over 80 characters as Betty would be flagged(:
+ *)
+ * Return: Success
+ **/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
