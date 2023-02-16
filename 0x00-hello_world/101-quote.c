6bfccb085bf97c0bd439f7432c3fc5c54a8977d5
TheSaviourEking <saviour.eking@outlook.com>
2023-02-16 00:53:49 -0800

    adds 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+#define STDERR 2
+/**
+*main - prints exactly "and that place of art is useful"
+* - Dora Korgar, 2015-10-19",
+*followed by a new line, to the standard error.
+*Return: Always 1(Not Success)
+*/
+int main(void)
+{
+    char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    write(STDERR, msg, sizeof(msg)-1);
+    return 1;
+}
