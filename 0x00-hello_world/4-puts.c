19fd1afc790e4d7ad4ea2ed2f25cd96ab66b7d4d
TheSaviourEking <saviour.eking@outlook.com>
2023-02-16 00:22:58 -0800

    add 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
-int main(void) 
+/**
+ * main - prints out the paranthesized text
+ * Description: This is a longer description.
+ * Don't forget that a line should not exceed 80 characters.
+ * But you're totally free to use several lines to properly
+ * describe your function
+ * Return: The paranthesized characters
+ */
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
