17d75bd8069f38d7ff0bd560f3824f46f7420e78
TheSaviourEking <saviour.eking@outlook.com>
2023-02-17 14:02:43 -0800

    add 100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all possible different combinations of three digits
+ *
+ * Return: Always (0) Success
+ */
+
+int main(void)
+{
+	for (int i = 0; i < 10; i++)
+	{
+		for (int j = i + 1; j < 10; j++)
+		{
+			putchar('0' + i);
+			putchar('0' + j);
+			if (i != 8 || j != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	return (0);
+}
