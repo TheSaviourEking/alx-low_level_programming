ee6135830e222b44a403df2538b068a78f4e6533
TheSaviourEking <saviour.eking@outlook.com>
2023-02-17 06:26:49 -0800

    add 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
+
 /**
  * main - prints alphabet in lowercase followed
  * by new line
+ *
  * Return: Always (0) Success
- **/
+ */
 int main(void)
 {
 	int i = 0;
+
 	for (i = 97; i <= 122; i++)
 	{
 		putchar(i);
 	}
+
 	putchar(10); /* This is the ascii for new line */
 
 	return (0);
