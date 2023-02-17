c5d0f6e9e32692e4dbbc6263e9984c28cdfabd3f
TheSaviourEking <saviour.eking@outlook.com>
2023-02-17 14:08:40 -0800

    add 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,9 +8,12 @@
 
 int main(void)
 {
-	for (int i = 0; i < 10; i++)
+	int i = 0;
+	int j = 0;
+
+	for (i = 0; i < 10; i++)
 	{
-		for (int j = i + 1; j < 10; j++)
+		for (j = i + 1; j < 10; j++)
 		{
 			putchar('0' + i);
 			putchar('0' + j);
