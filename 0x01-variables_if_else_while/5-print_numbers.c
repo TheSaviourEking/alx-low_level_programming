95d4212b73dd2426521beadc2bf55c3f05c20ece
TheSaviourEking <saviour.eking@outlook.com>
2023-02-17 07:32:26 -0800

    add 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all single numbers of base 10
+ * starting from zero (0)
+ *
+ * Return: Always (0) Success
+ */
+int main(void)
+{
+	int i = 0;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+
+return (0);
+}
