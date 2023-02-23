c5fa97a1602dfdabf67d270e5983a9c94aa76671
TheSaviourEking <saviour.eking@outlook.com>
2023-02-23 23:22:02 +0100

    adds 3-print_numbers.c and dependent file

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,7 +3,7 @@
 /**
  * print_numbers -> Prints the numbers from 0 to 9
  * followed by a new line
- * 
+ *
  * Return: Nothing
  */
 
@@ -11,7 +11,7 @@ void print_numbers(void)
 {
 	int i = 0;
 
-	for(i = 0; i < 10; i++)
+	for (i = 0; i < 10; i++)
 	{
 		_putchar(i + '0');
 	}
