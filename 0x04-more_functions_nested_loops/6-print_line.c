eed676eb9acdd40fe71500e25b05138ae46201ad
TheSaviourEking <saviour.eking@outlook.com>
2023-02-24 00:25:21 +0100

    adds 6-print_line.c and dependent file

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_line -> Draws a straight line in the terminal
+ * @n: number of time to print (_)
+ *
+ * Return: Nothing
+ * if n = 0, prints new line (\n)
+ */
+
+void print_line(int n)
+{
+	int i = 0;
+
+	for (i = 0; i < n; i++)
+	{
+		if (n <= 0)
+		{
+			_putchar('\n');
+		}
+
+		_putchar('-');
+	}
+	_putchar('\n');
+}
