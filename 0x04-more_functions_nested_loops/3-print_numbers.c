da1c7466eb8ce976870e9c99888ba35fb065c8a8
TheSaviourEking <saviour.eking@outlook.com>
2023-02-23 23:19:53 +0100

    adds 3-print_numbers.c and dependent file

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers -> Prints the numbers from 0 to 9
+ * followed by a new line
+ * 
+ * Return: Nothing
+ */
+
+void print_numbers(void)
+{
+	int i = 0;
+
+	for(i = 0; i < 10; i++)
+	{
+		_putchar(i + '0');
+	}
+	_putchar('\n');
+}
