d349204b37aaae9ece5b193ce78c5e52d5c47d28
TheSaviourEking <saviour.eking@outlook.com>
2023-02-27 15:01:08 +0100

    chore: adds 4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * print_rev -> Print a string in reverse,
+ * followed by a new line
+ * @s: the string to be printed
+ *
+ * Return: Nothing
+ */
+
+void print_rev(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+
+	while (len)
+	{
+		_putchar(s[--len]);
+	}
+
+	_putchar('\n');
+}
