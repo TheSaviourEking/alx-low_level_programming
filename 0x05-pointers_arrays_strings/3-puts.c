24b473060ec0461168c43d5e85c8b8f6e91f107a
TheSaviourEking <saviour.eking@outlook.com>
2023-02-27 09:25:21 +0100

    feature: adds 3-puts.c and dependencies

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts -> prints a string followed by new line to stdout
+ * @str: the string to be printed
+ *
+ * Return: Nothing
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
