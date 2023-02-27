71851fa364146e57739074fe05370c14b1184c72
TheSaviourEking <saviour.eking@outlook.com>
2023-02-27 16:08:19 +0100

    chore: adds 6-puts2.c and dependencies

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * puts2 -> Prints every other character of a string starting from the first
+ * character, followed by a new line
+ * @str: the sting
+ *
+ * Return: Nothing
+ */
+
+void puts2(char *str)
+{
+	int len = 0, i = 0;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	len = i - 1;
+
+	for (i = 0; i < len; i = i + 2)
+	{
+		_putchar(i + '0');
+	}
+	_putchar('\n');
+}
