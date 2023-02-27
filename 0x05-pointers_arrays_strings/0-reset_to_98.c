8fed465fa5103dc74a7d8803b48c727c01b54c0b
TheSaviourEking <saviour.eking@outlook.com>
2023-02-27 08:43:10 +0100

    ans: adds 0-main.c and dependent files

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 -> updates the value of the pointer to an
+ * int to 98
+ * @*n: the parameterbeing updated
+ *
+ * Return: Nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
