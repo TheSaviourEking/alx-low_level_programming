6452cfdb238588f9932c65b0c139034b84c305cd
TheSaviourEking <saviour.eking@outlook.com>
2023-02-28 04:38:57 +0100

    chore: adds 101-keygen.c  and dependencies

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -3,17 +3,18 @@
 #include <time.h>
 
  /**
- * main - program that generates random valid
+  * main  -> program that generates random valid
  * passwords for the program 101-crackme
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	int pass[100];
 	int i, sum, n;
-	
-	sum = 0;	
+
+	sum = 0;
 
 	srand(time(NULL));
 
