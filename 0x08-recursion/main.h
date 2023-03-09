beb1e8725801f48482e8a82e1711193151e91ae6
TheSaviourEking <saviour.eking@outlook.com>
2023-03-09 05:54:19 +0100

    chore: adds 100-is_palinfrome.c and dependent files

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -12,5 +12,7 @@ int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
 int _sqrt(int prev, int root);
+int helper_palindrome(char *s, int len);
+int _strlen_recursion(char *s);
 
 #endif
