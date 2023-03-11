a858a289c1af4fe164c8df9de3d0c2bfd1e4b9bd
TheSaviourEking <saviour.eking@outlook.com>
2023-03-11 20:56:00 +0100

    SOLVED: adds 3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * main -> multiplies two numbers
+ * @argc: Argument Count
+ * @argv: Argument Vector
+ *
+ * Return: 1 if no argument, and 0 otherwise
+ */
+
+int main(int argc, char *argv[])
+{
+	int i = 0, j = 0;
+
+	if (argc <= 1)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	}
+	return (0);
+}
