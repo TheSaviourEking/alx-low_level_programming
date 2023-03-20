f75a201f564984423a0a3f79694a96172ca3f44e
TheSaviourEking <saviour.eking@outlook.com>
2023-03-20 05:39:49 +0100

    SOLVED: adds 2-str_concat.c and TEST file

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,9 +15,13 @@ char *str_concat(char *s1, char *s2)
 	char *newString;
 	unsigned int i = 0, j = 0;
 
-	if (s1 == NULL || s2 == NULL)
+	if (s1 == NULL)
 	{
-		return (NULL);
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
 	}
 	else
 	{
