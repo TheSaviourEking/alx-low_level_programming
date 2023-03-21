2d75d63076c42e0a33ce64fef974badc67704abc
TheSaviourEking <saviour.eking@outlook.com>
2023-03-21 10:56:50 +0100

    SOLVED: adds 2-print_dog and 2-main.c test files

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,27 +1,22 @@
-#include "main.h"
-
+#include "dog.h"
+#include <stdio.h>
 /**
- * print_dog -> Prints a struct dog
- * @d: structure of a dog
- *
- * Return: Nothing
+ * print_dog - prints dog variables
+ * @d: struct of dog
  */
-
 void print_dog(struct dog *d)
 {
-	if (d == NULL)
+	if (d != NULL)
 	{
-		return;
-	}
-	if (d->name == NULL)
-	{
-		d->name = "(nil)";
-	}
+		if (d->name == NULL)
+			printf("Name: (nil)\n");
+		else
+			printf("Name: %s\n", d->name);
 
-	if (d->owner == NULL)
-	{
-		d->owner == NULL;
+		printf("Age: %f\n", d->age);
+		if (d->owner == NULL)
+			printf("Owner: (nil)\n");
+		else
+			printf("Owner: %s\n", d->owner);
 	}
-
-	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
