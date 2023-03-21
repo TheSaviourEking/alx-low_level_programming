23837b4403915815a9fa477e218d2b3d4396f26a
TheSaviourEking <saviour.eking@outlook.com>
2023-03-21 05:16:34 +0100

    SOLVED: adds dog.h and 0-main.c test file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,6 +14,12 @@ typedef struct dog
 	char *name;
 	float age;
 	char *owner;
-} dog;
+} dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+/* POSIBLE FILL */
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif /* DOG_H */
