ace7799dd075fd1a053a981e069a38cae45888e7
TheSaviourEking <saviour.eking@outlook.com>
2023-03-21 02:46:17 +0100

    SOLVED: adds 4-free_grid.c and TEST file

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * free_grid -> frees 2 dimensional grid created by "alloc_grid"
+ * @grid: matrix pointer
+ * @height: height
+ *
+ * Return: Nothing
+ */
+
+void free_grid(int **grid, int height)
+{
+	unsigned int i = 0;
+
+	if (height <= 0)
+	{
+		return;
+	}
+
+	if (grid == NULL)
+	{
+		return;
+	}
+
+	while (i < height)
+	{
+		free(grid[i]);
+		i++;
+	}
+	free(grid);
+}
