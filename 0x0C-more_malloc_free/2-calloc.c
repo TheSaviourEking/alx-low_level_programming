336d0d7ac964defb90f0de87bafcf84f71973518
TheSaviourEking <saviour.eking@outlook.com>
2023-03-21 13:47:55 +0100

    chore: adds 2-calloc.c and 2-main.c

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _calloc -> Allocates memory for an array
+ * @nmemb: number of elements in array
+ * @size: size(in bytes) of @nmemb elements
+ *
+ * Return: Nothing
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *p;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+
+	/* Malloc the area and zero it out */
+	else
+	{
+		p = malloc(nmemb * size);
+		bzero(p, nmemb * size);
+		return (p);
+	}
+}
