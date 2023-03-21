2fdd2ebdfd07378b8bd574a8598cf652fb081045
TheSaviourEking <saviour.eking@outlook.com>
2023-03-21 05:09:26 +0100

    chore: adds HEADER file

---
+++ 0x0E-structures_typedef/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* HEADER FILES */
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/* FUNCTIONS */
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+#endif /* MAIN_H */
