9fb4fc1fc686084c5cbf61e9926d1d2db558bd0e
TheSaviourEking <saviour.eking@outlook.com>
2023-03-23 08:43:19 +0100

    SOLVED: adds 2-int_index.c and 2-main.c (TEST)

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -20,14 +20,17 @@ int int_index(int *array, int size, int (*cmp)(int))
 		return (-1);
 	}
 
-	if (array != NULL && cmp != NULL)
+	if (size <= 0 || array == NULL || cmp == NULL)
 	{
-		for (i = 0; i < size; i++)
+		return (-1);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
 		{
-			if (cmp(array[i]))
-			{
-				return (i);
-			}
+			return (i);
 		}
 	}
 }
+
