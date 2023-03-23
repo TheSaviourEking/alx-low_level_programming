56fb27b8c005cdc7c4d006325198f418ac982ae4
TheSaviourEking <saviour.eking@outlook.com>
2023-03-23 15:04:21 +0100

    SOLVED: adds 2-int_index.c and 2-main.c (TEST)

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,23 +1,32 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "function_pointers.h"
+#include <stdlib.h>
+
 /**
- * int_index - indexes aray i think
- * @array: array to go through
- * @size: size of array
- * @cmp: function to do something with size
- * Return: int value
+ * int_index -> Functions that searches for an integer
+ * @array: An array parameter
+ * @size: Number of elements in array;
+ * @cmp: Pointer to function to be usedi
+ *
+ * Return: First element for which the "cmp" function does not return 0
+ * if no element matches, return (-1)
+ * if "size <= 0", return -1;
  */
+
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i;
+	int i = 0;
 
 	if (size <= 0 || array == NULL || cmp == NULL)
+	{
 		return (-1);
+	}
+
 	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]))
+		{
 			return (i);
+		}
 	}
 	return (-1);
 }
